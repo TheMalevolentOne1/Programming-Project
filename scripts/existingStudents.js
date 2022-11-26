e3d6b31173c7f91eaee2c7b7e025390d828098f3
TheMalevolentOne1 <kylerobinson818@gmail.com>
2022-11-26 21:35:46 +0000

    update (connect PHP file to required things)

--- scripts/existingStudents.js
+++ scripts/existingStudents.js
@@ -1,10 +1,8 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+let sql = "SELECT * FROM tblStudents"
+fetch(`D:\XAMPP\htdocs\PHP Project File\New folder\database.php?sql=${sql}`, {method:"GET"}).then(res => {
+	if (!res.ok) {
+		alert("OKAY")
+	} else {
+		alert(res)
+	}
+})
