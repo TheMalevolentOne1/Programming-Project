ecf70023abc5567f0ba6be95dc9c6683382ac833
TheMalevolentOne1 <kylerobinson818@gmail.com>
2022-11-27 17:54:55 +0000

    things

--- scripts/existingStudents.js
+++ scripts/existingStudents.js
@@ -1,13 +1,10 @@
+let sql = "SELECT * FROM tblStudents"
+fetch("../database.php?sql="+sql, {method:"GET"}).then(res => {
+	if (!res.ok) { alert("Error, Something went wrong!"); console.log(res); return; }
+}).catch(err => { alert(err); })
+
 const sqlfunc = (data) => {
-	const tab = document.createElement("div");
-	tab.setAttribute("class", "PHPTEST");
-
+	const item = document.createElement("div");
 	
-}
-
-window.onload = () => {
-	let sql = "SELECT * FROM tblStudents"
-	fetch("../database.php?sql="+sql, {method:"GET"}).then(res => {
-		if (!res.ok) { alert("Error, Something went wrong!"); console.log(res); return; }
-	}).catch(err => { alert(err); })
+	document.body.appendChild(item);
 }
